8d1d21166a8d31864b4078cfe38b883339a9bcd1
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-16 22:07:24 +0800

    Pseudocode so far

---
+++ COVID_Contact_Tracing_Program.py
@@ -0,0 +1,5 @@
+# Create a simple GUI for COVID contract tracing
+# Make a simple program wherein the user will able to input all the information
+# collect all the data
+# print all the data into a txt file
+# add a feature wherein the user may search for all the entries 
