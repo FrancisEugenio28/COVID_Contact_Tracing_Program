2021ba0bb2b83717122cc11656a2d85995b5e0ae
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-17 10:06:54 +0800

    Final Pseudocode so far

--- COVID_Contact_Tracing_Program.py
+++ COVID_Contact_Tracing_Program.py
@@ -1,5 +1,19 @@
 # Create a simple GUI for COVID contract tracing
 # Make a simple program wherein the user will able to input all the information
+    # Input name
+    # Input age
+    # Input Address
+    # Input Phone number
+    # Input Email
+    # Ask the user if they are vaccinated
+    # Ask the users if they experience any symptoms of the disease for the past weeks
+    # Ask the users if they been with a person with possible symptoms
+        #(PARENT OR GUARDIAN INFORMATION)
+        # Input name
+        # Input Relationship to the Guardian/Parent
+    # create a submit button 
 # collect all the data
-# print all the data into a txt file
+    # create a data base 
+    # print all the data into a txt file 
+    # store it in the data base
 # add a feature wherein the user may search for all the entries 
