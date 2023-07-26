67266acf1f357938c4f76ae9bb5118e9f4a17eee
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-26 14:28:48 +0800

    initial start for the search information part

---
+++ Search_Information.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+# create a class 
+class search_information:
+    # create a GUI for seach info
+    def __init__(self):
+        self.search_gui = Tk()
+        self.search_gui.title("Search Your Entry")
+        self.search_gui.geometry('400x400')
+
+# add function for each entry
+# read the csv for possible entry
+# display the entry 
+        self.search_gui.mainloop()
