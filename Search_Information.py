f6a5c11fd93bcf29445f81b0dc9eed330d1193a1
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-27 14:04:26 +0800

    still trying to figure out how to fix the submit button for the toplevel function

--- Search_Information.py
+++ Search_Information.py
@@ -7,14 +7,13 @@ import csv
 # create a class 
 class search_information:
     def __init__(self):
-# create a GUI for seach
+        # create a GUI for search
         self.search_gui = Tk()
         self.search_gui.title("Search Your Entry")
         self.search_gui.geometry('550x245')
 
-        # Set the width of the head_frame to the width of the search_gui
-        self.head_frame = Frame(self.search_gui, bd=2, relief="groove", bg="#FF0000")  # Stylish red color
-        self.head_frame.place(x=5, y=5, width=540, height=60)  # Updated width to 390
+        self.head_frame = Frame(self.search_gui, bd=2, relief="groove", bg="#FF0000")
+        self.head_frame.place(x=5, y=5, width=540, height=60)
         self.head_label = Label(self.head_frame, text="CONTACT TRACING 2023-2024", font=("Helvetica", 20, "bold"), bg="#FF0000", fg="white")
         self.head_label.pack(pady=10)
 
@@ -22,124 +21,123 @@ class search_information:
         padding_frame.place(x=5, y=70, width=540, height=170)
 
         self.referencenum_label = Label(self.search_gui, text="PLEASE ENTER YOUR REFERENCE NUMBER: ")
-        self.referencenum_label.place(x=15,y=100)
+        self.referencenum_label.place(x=15, y=100)
         self.referencenum_entry = Entry(self.search_gui, width='35')
-        self.referencenum_entry.place(x=255,y=100)
+        self.referencenum_entry.place(x=255, y=100)
+
+        # create an instance of ForgotRefNum
+        self.forgot_ref_num = ForgotRefNum()
 
         # create a forget button for those who forget their reference number
-        self.forget_text = Label(self.search_gui, text='Note: in case you forgot your own reference number please click "Forgot Reference Number"\r for other ways of searching your inforamtion')
-        self.forget_text.place(x=25,y=130)
-        self.forget_btn = Button(self.search_gui, text='Forgot Reference Number', command=self.create_new_tab)
-        self.forget_btn.place(x=190,y=175)
+        self.forget_text = Label(self.search_gui, text='Note: in case you forgot your own reference number please click "Forgot Reference Number"\r for other ways of searching your information')
+        self.forget_text.place(x=25, y=130)
+        self.forget_btn = Button(self.search_gui, text='Forgot Reference Number', command=self.forgot_ref_num.create_new_tab)
+        self.forget_btn.place(x=190, y=175)
 
         # create a button for search
         self.search_btn = Button(self.search_gui, text='Search', command=self.search_entry)
-        self.search_btn.place(x=480,y=97)
+        self.search_btn.place(x=480, y=97)
 
         self.data_list = self.read_csv()
 
         self.search_gui.mainloop()
 
     # add function for forgot button
+class ForgotRefNum:
     def create_new_tab(self):
-        # Function to create another tab when the "Forgot Reference Number" button is pressed
-        self.new_tab = Toplevel(self.search_gui)
-        self.new_tab.title("Other way to see your information")
-        self.new_tab.geometry('820x325')
-        # heading
-        self.head_frame = Frame(self.new_tab, bd=2, relief="groove", bg="#FF0000")  # Stylish red color
-        self.head_frame.place(x=5, y=5, width=810, height=60)
-        self.head_label = Label(self.head_frame, text="CONTACT TRACING 2023-2024", font=("Helvetica", 20, "bold"), bg="#FF0000", fg="white")
-        self.head_label.pack(pady=10)
-        # padding
-        self.padding_frame = Frame(self.new_tab, bd=2, relief="groove")
-        self.padding_frame.place(x=5, y=70, width=810, height=250)
-        # instruction
-        self.instruction_label = Label(self.new_tab, text="This pannel will be the other way for you to confirm that you've entered your information in the application. May you please fill all the needed \rinformation")
-        self.instruction_label.place(x=25,y=80)
-        # input last name
-        lastname_label = Label(self.new_tab,text = "LAST NAME : ")
+        new_tab = Toplevel(self.search_gui)
+        new_tab.title("Other way to see your information")
+        new_tab.geometry('820x325')
+
+        head_frame = Frame(new_tab, bd=2, relief="groove", bg="#FF0000")
+        head_frame.place(x=5, y=5, width=810, height=60)
+        head_label = Label(head_frame, text="CONTACT TRACING 2023-2024", font=("Helvetica", 20, "bold"), bg="#FF0000", fg="white")
+        head_label.pack(pady=10)
+
+        padding_frame = Frame(new_tab, bd=2, relief="groove")
+        padding_frame.place(x=5, y=70, width=810, height=250)
+
+        instruction_label = Label(new_tab, text="This panel will be the other way for you to confirm that you've entered your information in the application. May you please fill all the needed information")
+        instruction_label.place(x=25,y=80)
+
+        lastname_label = Label(new_tab,text = "LAST NAME : ")
         lastname_label.place(x=10, y=120)
-        self.lastname_entry = Entry(self.new_tab,width="28") 
+        self.lastname_entry = Entry(new_tab,width="28") 
         self.lastname_entry.place(x=90,y=120)
-        # input middle name
-        middlename_label = Label(self.new_tab,text = "MIDDLE NAME : ")
+
+        middlename_label = Label(new_tab,text = "MIDDLE NAME : ")
         middlename_label.place(x=270, y=120)
-        self.middlename_entry = Entry(self.new_tab,width="28") 
+        self.middlename_entry = Entry(new_tab,width="28") 
         self.middlename_entry.place(x=365,y=120)
-        # input first name
-        firstname_label = Label(self.new_tab,text = "FIRST NAME : ")
+
+        firstname_label = Label(new_tab,text = "FIRST NAME : ")
         firstname_label.place(x=545, y=120)
-        self.firstname_entry = Entry(self.new_tab,width="28") 
+        self.firstname_entry = Entry(new_tab,width="28") 
         self.firstname_entry.place(x=625,y=120)
-        # age
-        age_label = Label(self.new_tab, text= "AGE : ")
+
+        age_label = Label(new_tab, text= "AGE : ")
         age_label.place(x=10, y=160)
-        self.age_entry = Scale(self.new_tab, from_=1, to=100, length="730",orient="horizontal")
+        self.age_entry = Scale(new_tab, from_=1, to=100, length="730",orient="horizontal")
         self.age_entry.place(x=65,y=140)
-        # address
-        address_label = Label(self.new_tab, text="ADDRESS : ")
+
+        address_label = Label(new_tab, text="ADDRESS : ")
         address_label.place(x=10,y=190)
-        self.address_entry = Entry(self.new_tab, width=120)
+        self.address_entry = Entry(new_tab, width=120)
         self.address_entry.place(x=75,y=190)
-        # contact number
-        contactnum_label = Label(self.new_tab, text="CONTACT NUMBER : ")
+
+        contactnum_label = Label(new_tab, text="CONTACT NUMBER : ")
         contactnum_label.place(x=10,y=220)
-        self.contactnum_entry = Entry(self.new_tab, width='45')
+        self.contactnum_entry = Entry(new_tab, width='45')
         self.contactnum_entry.place(x=130,y=220)
-        # gender
-        gender_label = Label(self.new_tab, text="GENDER : ")
+
+        gender_label = Label(new_tab, text="GENDER : ")
         gender_label.place(x=410,y=220)
-        self.gender_entry = ttk.Combobox(self.new_tab, width="50",values=["Prefer Not to Say", "Male", "Female", "LGBTQ+", "Other"])
+        self.gender_entry = ttk.Combobox(new_tab, width="50", values=["Prefer Not to Say", "Male", "Female", "LGBTQ+", "Other"])
         self.gender_entry.place(x=475,y=220)
-        # email
-        email_label = Label(self.new_tab, text="EMAIL : ")
+
+        email_label = Label(new_tab, text="EMAIL : ")
         email_label.place(x=10,y=250)
-        self.email_entry = Entry(self.new_tab, width=122)
+        self.email_entry = Entry(new_tab, width=122)
         self.email_entry.place(x=60,y=250)
-        
-        # gather all the inputs 
-        def search_info(self):
-            last_name = self.lastname_entry.get().upper()
-            middle_name = self.middlename_entry.get().upper()
-            first_name = self.firstname_entry.get().upper()
-            age = self.age_entry.get()
-            address = self.address_entry.get().upper()
-            contact_number = self.contactnum_entry.get().upper()
-            gender = self.gender_entry.get().upper()
-            email = self.email_entry.get().upper()
-        # read csv is there is a same input
-            with open("contact_tracing_data.csv", "r") as csvfile:
-                csv_reader = csv.DictReader(csvfile)
-                for row in csv_reader:
-                    if (
-                        row["LAST_NAME"] == last_name
-                        and row["MIDDLE_NAME"] == middle_name
-                        and row["FIRST_NAME"] == first_name
-                        and row["AGE"] == age
-                        and row["ADDRESS"] == address
-                        and row["CONTACT_NUMBER"] == contact_number
-                        and row["GENDER"] == gender
-                        and row["EMAIL"] == email
-                    ):
-                        # print your info
-                        print("Your information:")
-                        print("Last Name:", row["LAST_NAME"])
-                        print("Middle Name:", row["MIDDLE_NAME"])
-                        print("First Name:", row["FIRST_NAME"])
-                        print("Age:", row["AGE"])
-                        print("Address:", row["ADDRESS"])
-                        print("Contact Number:", row["CONTACT_NUMBER"])
-                        print("Gender:", row["GENDER"])
-                        print("Email:", row["EMAIL"])
-                        break
-                else:
-                    # if no match is found, inform the user
-                    print("No matching information found.")
-        # create a search button
-        search_btn = Button(self.new_tab, text="Search", command=search_info)
+
+        search_btn = Button(new_tab, text="Search", command=self.search_info)
         search_btn.place(x=375,y=280)
-        # print your info
+
+    def search_info(self):
+        last_name = self.lastname_entry.get().upper()
+        middle_name = self.middlename_entry.get().upper()
+        first_name = self.firstname_entry.get().upper()
+        age = self.age_entry.get()
+        address = self.address_entry.get().upper()
+        contact_number = self.contactnum_entry.get().upper()
+        gender = self.gender_entry.get().upper()
+        email = self.email_entry.get().upper()
+
+        with open("contact_tracing_data.csv", "r") as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            for row in csv_reader:
+                if (
+                    row["LAST_NAME"] == last_name
+                    and row["MIDDLE_NAME"] == middle_name
+                    and row["FIRST_NAME"] == first_name
+                    and row["AGE"] == age
+                    and row["ADDRESS"] == address
+                    and row["CONTACT_NUMBER"] == contact_number
+                    and row["GENDER"] == gender
+                    and row["EMAIL"] == email
+                ):
+                    print("Your information:")
+                    print("Last Name:", row["LAST_NAME"])
+                    print("Middle Name:", row["MIDDLE_NAME"])
+                    print("First Name:", row["FIRST_NAME"])
+                    print("Age:", row["AGE"])
+                    print("Address:", row["ADDRESS"])
+                    print("Contact Number:", row["CONTACT_NUMBER"])
+                    print("Gender:", row["GENDER"])
+                    print("Email:", row["EMAIL"])
+                    break
+            else:
+                print("No matching information found.")
 
         # Add contents to the forgot the reference button choice
     def create_info_tab(self):
