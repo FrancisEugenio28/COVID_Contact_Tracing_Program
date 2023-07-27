499174ac2b5377f2bbc376cf1f79beb34bce8449
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-27 21:07:53 +0800

    done for the initial GUI interface for the main menu

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from tkinter import *
 # import all the class
+from COVID_Contact_Tracing_Program import registration
+from Search_Information import search_information
 # create a simple GUI for main menu
+main_menu = Tk()
+main_menu.title("Contact Tracing 2023-2024")
+main_menu.geometry("500x300")
 # header
 # padding
 # create a button for registration
 # create a button for search information
 # run main menu class
+main_menu.mainloop()
